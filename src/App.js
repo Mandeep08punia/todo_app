c2fcd024320e4ca2ddb0c15aa320a885ab3738d9
Mandeep Punia <48849295+Mandeep08punia@users.noreply.github.com>
2021-03-31 16:03:58 +0530

    almost all functionality added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React,{useState} from "react";
-import {Button, FormControl, FormHelperText, Input, InputLabel} from "@material-ui/core";
+import React,{useEffect, useState} from "react";
+import {Button, FormControl,  Input, InputLabel} from "@material-ui/core";
 import './App.css';
 import Todo from "./Todo";
+import db from "./firebase"
+import firebase from "firebase"
 
 function App() {
-  const [todos,setTodods]=useState(['Take dogs for a walk','mandeep punia','Take the rubbish out']);
+  const [todos,setTodos]=useState([]);
   const [input,setInput]=useState('');
-  console.log(''+input);
+  
+  // when the app loads ,I need to listen to the database and fetch new todos as they get added/removed
+  useEffect(()=>{
+    // this code will fire when the app.js load
+    db.collection('todos').orderBy('timestamp','desc').onSnapshot(snapshot=>{
+      //console.log(snapshot.docs.map(doc=>doc.data().todo));
+      setTodos((snapshot.docs.map(doc=>({id:doc.id,todo:doc.data().todo}))))
+    })
+  },[]);
+
   const addToDo=(event)=>{
     //this will fire off when we click the button
     event.preventDefault();//It will stop the refresh
-    console.log(''+"i am working");
-    setTodods([...todos,input]);
+    db.collection('todos').add({
+      todo:input,
+      timestamp:firebase.firestore.FieldValue.serverTimestamp()
+    })
+    
     setInput('');//clear up the input after clicking add todo button
-    console.log(todos);
+    
   }
 
   return (
     <div className="App">
+    <div className="App_style">
     <h1> To-do app</h1>
+    <h5 >Built by Mandeep Punia</h5>
     <form>
     <FormControl>
     <InputLabel >Write a Todo</InputLabel>
@@ -33,10 +49,10 @@ function App() {
     </form>
     <ul>
       {todos.map(todo=>(
-        <Todo text={todo}/>
-        //<li>{todo}</li>
+        <Todo todo={todo}/>        
       ))}
-    </ul>   
+    </ul>
+    </div>   
     </div>
   );
 }
